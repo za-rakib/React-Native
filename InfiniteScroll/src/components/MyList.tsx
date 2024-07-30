c7a035689451814cb73f17bc0d17d497a31d0674
Rakib <rakib@sayburgh.com>
2024-07-30 13:27:55 +0600

    pagination setup

--- InfiniteScroll/src/components/MyList.tsx
+++ InfiniteScroll/src/components/MyList.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-import {ActivityIndicator, FlatList, Text} from 'react-native';
+import {ActivityIndicator, FlatList, Text, View} from 'react-native';
 import character from '../data/character.json';
 import CharacterListItem from './CharacterListItem';
 import {useEffect, useState} from 'react';
@@ -7,6 +7,7 @@ import {useEffect, useState} from 'react';
 const MyList = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [nextPage, setNextPage] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -14,18 +15,40 @@ const MyList = () => {
       const response = await fetch('https://rickandmortyapi.com/api/character');
       const responseJson = await response.json();
       setItems(responseJson.results);
+      setNextPage(responseJson.info.next);
       setLoading(false);
     };
     fetchItems();
   }, []);
-  if (loading) {
-    return <ActivityIndicator size="large" color="#00ff00" />;
-  }
 
+  const loadMore = async () => {
+    const fetchItems = async () => {
+      setLoading(true);
+      const response = await fetch(nextPage);
+      const responseJson = await response.json();
+      setItems(exisingItems => {
+        return [...exisingItems, ...responseJson.results];
+      });
+      setNextPage(responseJson.info.next);
+      setLoading(false);
+    };
+    fetchItems();
+  };
   return (
     <FlatList
       data={items}
       renderItem={({item}) => <CharacterListItem character={item} />}
+      contentContainerStyle={{gap: 10}}
+      ListFooterComponent={() => (
+        <View>
+          {loading && <ActivityIndicator size="large" />}
+          <Text
+            onPress={loadMore}
+            style={{alignSelf: 'center', fontSize: 18, color: 'blue'}}>
+            Load More
+          </Text>
+        </View>
+      )}
     />
   );
 };
