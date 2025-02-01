3bc3bf64136c9cc10e936e47878467da94a42e5b
Md Rakib Hossen <rakib.dev70@gmail.com>
2025-02-01 21:27:28 +0600

    boilerplate set up

--- EchoTune/src/app/index.tsx
+++ EchoTune/src/app/index.tsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const Index = () => {
 	return (
-		<View>
-			<Text>indextttt</Text>
+		<View style={styles.container}>
+			<Text style={styles.name}>Abdullah </Text>
 		</View>
 	)
 }
 
-export default index
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#f5f5f5', // Light gray background
+	},
+	name: {
+		fontSize: 28,
+		fontWeight: 'bold',
+		color: '#2e86de', // Blue shade
+		backgroundColor: '#fff',
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		borderRadius: 10,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 4 },
+		shadowOpacity: 0.3,
+		shadowRadius: 4,
+		elevation: 5, // Shadow effect for Android
+	},
+})
+
+export default Index
