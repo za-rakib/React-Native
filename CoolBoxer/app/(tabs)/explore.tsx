9771846a825d342e95d789844b6d99ff611dedf0
Md Rakib Hossen <rakib.dev70@gmail.com>
2025-05-01 21:48:11 +0600

    :tada: Project start

---
+++ CoolBoxer/app/(tabs)/explore.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const explore = () => {
+  return (
+    <View>
+      <Text>explore</Text>
+    </View>
+  )
+}
+
+export default explore
---
+++ CoolBoxer/app/_layout.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const _layout = () => {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
+}
+
+export default _layout
